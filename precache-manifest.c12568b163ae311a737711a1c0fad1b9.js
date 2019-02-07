b64b256d316bf2b34044d5abcaf3d4755c1bd33f
Dmytro Podolianskyi <podolyanskiidmi@gmail.com>
2019-02-07 02:59:47 +0200

    Updates

---
+++ precache-manifest.c12568b163ae311a737711a1c0fad1b9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "508f285d2d6ea1c47661",
+    "url": "/dmytryi4/lastfm-react-client/static/css/3.acba245a.chunk.css"
+  },
+  {
+    "revision": "5cb828b7030d0d4a43b2",
+    "url": "/dmytryi4/lastfm-react-client/static/js/0.5cb828b7.chunk.js"
+  },
+  {
+    "revision": "ca502033d0330590f0ae",
+    "url": "/dmytryi4/lastfm-react-client/static/js/main.ca502033.chunk.js"
+  },
+  {
+    "revision": "b8c0d2837bf54faacb9a",
+    "url": "/dmytryi4/lastfm-react-client/static/js/runtime~main.b8c0d283.js"
+  },
+  {
+    "revision": "05af6e65781cb8e529c0",
+    "url": "/dmytryi4/lastfm-react-client/static/js/2.05af6e65.chunk.js"
+  },
+  {
+    "revision": "4317bc6f5d27ad50851a",
+    "url": "/dmytryi4/lastfm-react-client/static/js/5.4317bc6f.chunk.js"
+  },
+  {
+    "revision": "508f285d2d6ea1c47661",
+    "url": "/dmytryi4/lastfm-react-client/static/js/3.508f285d.chunk.js"
+  },
+  {
+    "revision": "2c58ffc985a1d987ed2f",
+    "url": "/dmytryi4/lastfm-react-client/static/js/4.2c58ffc9.chunk.js"
+  },
+  {
+    "revision": "ca502033d0330590f0ae",
+    "url": "/dmytryi4/lastfm-react-client/static/css/main.19c4669d.chunk.css"
+  },
+  {
+    "revision": "05af6e65781cb8e529c0",
+    "url": "/dmytryi4/lastfm-react-client/static/css/2.6a06b2c2.chunk.css"
+  },
+  {
+    "revision": "b412423c5020bdbbf9642e2d4023f35b",
+    "url": "/dmytryi4/lastfm-react-client/index.html"
+  }
+];
